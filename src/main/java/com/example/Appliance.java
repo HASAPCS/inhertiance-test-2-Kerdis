1e477b83ff2c0f847b5d97912b835798893d8cb4
Kerdis <kerdispan1234@gmail.com>
2023-11-23 10:28:39 +0800

    yes ma'am

--- src/main/java/com/example/Appliance.java
+++ src/main/java/com/example/Appliance.java
@@ -1,39 +1,52 @@
-package com.example;
 
-public class Appliance {
-    
-
-    // Constructor to initialize an ElectronicDevice object
-    public Appliance(String brand, String model) {
-        // TODO: Initialize the common attributes (brand, model) with the provided parameters
-    }
-
-    // TODO: Implement getters (and setters if necessary) for the brand and model.
-
-    // OPTIONALLY implement the required methods to pass the BonusTest.
-
-}
 
 package com.example;
 
 public class Appliance {
     // Attributes
     // TODO: Declare common attributes for electronic devices, such as brand and model and powerUsage.
+    private String brand;
+    private String model;
+    private int powerUsage;
 
     // Constructor
     public Appliance(String brand, String model, int powerUsage) {
         // TODO: Initialize all attributes
+        this.brand = brand;
+        this.model = model;
+        this.powerUsage = powerUsage;
     }
 
     // Getters
     // TODO: Implement getters for brand, model, and powerUsage
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public int getPowerUsage() {
+        return this.powerUsage;
+    }
 
     // displayInfo Method
     // TODO: Implement a method that returns information about the appliance, including brand, model, and power usage.
+    public String displayInfo() {
+        return "Brand: " + getBrand() + ", " + "Model: " + getModel() + ", " + "Power Usage: " + getPowerUsage() + " Watts";
+    }
+
     // Check the tests for more details
 
     // isEcoFriendly Method
     // TODO: Implement a method to check if the appliance is eco-friendly based on its power usage. 
+    public String isEcoFriendly() {
+        if (powerUsage > 400) {
+            return ("The displayInfo method does not return the correct information.");
+        }
+        return ("Not eco friendly.");
+    }
     // An appliance that uses less than 400 Watts of power is eco-friendly.
 
 }
--- src/main/java/com/example/Refrigerator.java
+++ src/main/java/com/example/Refrigerator.java
@@ -2,18 +2,33 @@ package com.example;
 
 // TODO: make this class extend Appliance
 
-public class Refrigerator {
+public class Refrigerator extends Appliance{
     // Additional attributes
     private int temperature;
-
     // Constructor
     // TODO: Initialize the Refrigerator with brand, model, power usage, temperature
+    public Refrigerator(String brand, String model, int powerUsage, int temperature) {
+        super(brand, model, powerUsage);
+        this.temperature = temperature;
+    }
+
+    public int getTemperature() {
+        return this.temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
 
     // Override displayInfo Method from Appliance
     // TODO: Implement a method that extends the information display to include the refrigerator's specific details (temperature)
+    public String displayInfo() {
+        return super.displayInfo() + ", " + "Temperature: " + getTemperature() + "°C";
+    }
 
     // setTemperature Method
     // TODO: Implement a method to set the temperature of the refrigerator
+    
 
 
 
